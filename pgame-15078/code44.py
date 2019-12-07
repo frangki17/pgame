1ab2b8c56bdabeddf7fd4557992e50cf3c622b83
frangki17 <56684068+frangki17@users.noreply.github.com>
2019-12-08 01:56:31 +0900

    Create code44.py
    Praktikum 4

---
+++ pgame-15078/code44.py
@@ -0,0 +1,35 @@
+#buat variabel dan berikan elemen
+buah=["Durian", "Mangga","Rambutan","Anggur","Pepaya"]
+
+#cetak list buah
+print(buah)
+
+#cetak elemen pertama list (elemen pertama adalah 0)
+print("Buah [0] ", buah[0])
+
+#cetak elemen kedua list (elemen kedua adalah 1)
+print("Buah [1] ", buah[1])
+
+#cetak elemen ketiga list sampai akhir elemen
+print("Buah [2] ", buah[2])
+
+#cetak elemen terakhir list ( [-1] adalah elemen terakhir)
+print("buah [-1] ", buah[-1])
+
+print('*'*35)
+
+#buat variabel list mylist dan berikan elemen
+mylist=["Ternate", 1000, 2000, "Tidore", "Maitara", 3000]
+
+#cetak list
+print("mylist : ", mylist)
+
+#cetak list elemen pertama sampai elemen ketiga
+print("mylist [:3] ", mylist[:3])
+
+#cetak list elemen kedua dan tiga
+print("mylist[1] : ", mylist[1])
+print("mylist[2] : ", mylist[2])
+
+#operasi antar elemen list : elemen ke 2 + elemen ke 3
+print("mylist[1] + mylist[2] : ", mylist[1] + mylist[2])
