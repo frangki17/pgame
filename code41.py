f02690bb294056514ccc75f3035abfe2d79e20be
frangki17 <56684068+frangki17@users.noreply.github.com>
2019-12-08 00:16:54 +0900

    Create code41.py

---
+++ code41.py
@@ -0,0 +1,31 @@
+#NUMERIK dan INTEGER
+#buat variabel
+
+a = 4
+b = 5
+c = 6.3
+
+
+
+#cetak ke layar
+
+print(a)
+print(b)
+print(c)
+
+#cetak '*' sebanyak 35 kali 
+print('*'*35)
+
+#cetak variabel dengan keterangan ke layar
+
+print("variabel A :", a )
+print("Variabel B : %d " % b )
+print("Variabel C : %f " % c )
+print('*'*35)
+
+#cetak variabel dengan keterangan ke layar
+#operasi + , * , / dan cetak ke layar
+
+print("Variabel A+B : ", a+b )
+print("Variabel A*B : ", a*b )
+print("Variabel A/B : ", c/b )
