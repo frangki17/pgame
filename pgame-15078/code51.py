67d5971f6232a07f8d32f71aff4d8647a4526d1a
frangki17 <56684068+frangki17@users.noreply.github.com>
2019-12-08 02:05:14 +0900

    Create code51.py
    Praktikum 5

---
+++ pgame-15078/code51.py
@@ -0,0 +1,23 @@
+#latihan 5.1 list lanjutan
+#praktikum 5
+
+
+#buat variabel dan berikan elemen
+buah=["Durian", "Mangga", "Rambutan"]
+
+#print jumlah elemen
+print("Jumlah Element :", len(buah))
+
+
+#tambah elemen yang sama 
+buah.append("Mangga")
+
+#hitung elemen "Mangga" hasil 2
+print("Jumlah Buah Mangga :", buah.count("Mangga"))
+
+
+#tambah list ke dalam list buah
+buah.append(["Salak", "Nangka", "Apel"])
+
+#print list buah setelah ditambah
+print("List Buah :", buah)
