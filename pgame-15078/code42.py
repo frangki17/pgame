d9ed38c82e7c15d44f7d0c17b09130825e54799a
frangki17 <56684068+frangki17@users.noreply.github.com>
2019-12-08 01:51:02 +0900

    Create code42.py
    Praktikum 4

---
+++ pgame-15078/code42.py
@@ -0,0 +1,24 @@
+prodi="Informatika UMMU"
+
+print(prodi)
+
+#cetak kolom 0 (pertama) dari variabel prodi
+print(prodi[0])
+
+#cetak kolom 0 dengan keterangan
+print("prodi [0] :", prodi[0])
+
+#cetak kolom awal sampai sampai kolom ke 4
+print(prodi[:3])
+
+#cetak kolom awal sampai kolom ke 4 dengan keterangan
+print("prodi [:3] :", prodi[:3])
+
+#cetak semua kecuali kolom 0 (pertama)
+print(prodi[1:])
+
+#cetak semua kecuali kolom terakhir
+print(prodi[:-1])
+
+#cetak semua kecuali kolom terakhir dengan keterangan
+print("prodi [:-2] : ", prodi[:-2])
