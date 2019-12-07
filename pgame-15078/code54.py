bc029369556267b192a4b20eaf3ca8366745ce71
frangki17 <56684068+frangki17@users.noreply.github.com>
2019-12-08 02:14:26 +0900

    Create code54.py
    Praktikum 5

---
+++ pgame-15078/code54.py
@@ -0,0 +1,13 @@
+#latihan code 5.4 : Sets
+
+#buat variabel
+buah = {'Durian', 'Mangga', 'Salak', 'Mangga', 'Salak',
+		'Durian'}
+print("Element Set :", buah)
+
+#tambah elemen
+buah.add("Melon")
+
+#tambah lebih dari satu elemen
+buah.update(["Pepaya", "Semangka"])
+print("Element Set :", buah)
