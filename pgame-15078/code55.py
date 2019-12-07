ca9d1435648e7a5cf4344d11fa134d2ee427b23a
frangki17 <56684068+frangki17@users.noreply.github.com>
2019-12-08 02:17:15 +0900

    Create code55.py
    Praktikum 5

---
+++ pgame-15078/code55.py
@@ -0,0 +1,23 @@
+#latihan code 5.5 : Kondisional
+
+#Buat Variabel A dan B
+A = 50
+B = 20
+
+#gunakan kondisi if
+if A > B:
+	print("A lebih besar dari B")
+
+#gunakan if, elif
+if A > B:
+	print("A lebih besar dari B")
+elif A == B:
+	print("A sama dengan B")
+
+#gunakan if, elif,else
+if A > B:
+	print("A lebih besar dari B")
+elif A == B:
+	print("A sama dengan B")
+else:
+	print("A Lebih Kecil dari B")
